f8c4f79837a162edd8ab3b0b7f7d91d0905a738a
Simulnandasara <165984077+Simulnandasara@users.noreply.github.com>
2024-04-05 19:27:02 +0530

    index search

---
+++ indexsearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int findIndex(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+   
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 7, 9, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int index = findIndex(arr, n, target);
+
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", target, index);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+
+    return 0;
+}
