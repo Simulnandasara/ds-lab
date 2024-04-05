59fe423dbf7f045a472cf1d003ba94d50453a23e
Simulnandasara <165984077+Simulnandasara@users.noreply.github.com>
2024-04-05 18:59:29 +0530

    Update SELECTIO.C
    chande code

--- SELECTIO.C
+++ SELECTIO.C
@@ -1,39 +1,40 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
-    
-    for (i = 0; i < n - 1; i++) {
-        minIndex = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
+void selection(int arr[], int n)
+{
+    int i, j, small;
+
+    for (i = 0; i < n - 1; i++) 
+    {
+        small = i; //minimum element in unsorted array
+
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[small])
+                small = j;
+ 
+        {
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
         }
-        temp = arr[minIndex];
-        arr[minIndex] = arr[i];
-        arr[i] = temp;
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+void printArr(int a[], int n) /* function to print the array */
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
 }
 
-int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Original array: \n");
-    printArray(arr, n);
-    
-    selectionSort(arr, n);
-    
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    
+int main()
+{
+    int a[] = {12, 31, 25, 8, 32, 17};
+    int n = sizeof(a) / sizeof(a[0]);
+    printf("Before sorting array elements are - \n");
+    printArr(a, n);
+    selection(a, n);
+    printf("\nAfter sorting array elements are - \n");
+    printArr(a, n);
     return 0;
 }
